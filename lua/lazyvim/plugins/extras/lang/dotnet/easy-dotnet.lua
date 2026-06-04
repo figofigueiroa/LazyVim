12e236ad8cfe879d0e122e69802538b4a4c36c51
Thiago Figueiroa Oliveira <thiagofigueiroaoliveira@gmail.com>
2026-06-04 09:01:08 -0300

    adiciona parser do c_sharp ao treesitter no extre

--- lua/lazyvim/plugins/extras/lang/dotnet/easy-dotnet.lua
+++ lua/lazyvim/plugins/extras/lang/dotnet/easy-dotnet.lua
@@ -1,8 +1,14 @@
 return {
-  -- lazy.nvim
-  "GustavEikaas/easy-dotnet.nvim",
-  dependencies = { "nvim-lua/plenary.nvim", "folke/snacks.nvim" },
-  config = function()
-    require("easy-dotnet").setup()
-  end,
+  { -- lazy.nvim
+    "GustavEikaas/easy-dotnet.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "folke/snacks.nvim" },
+    config = function()
+      require("easy-dotnet").setup()
+    end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "c_sharp", "fsharp" } },
+  },
 }
