44c0c44f2ec6c28919e3f1fd8e04309b926746f1
Thiago Figueiroa Oliveira <thiagofigueiroaoliveira@gmail.com>
2026-06-04 09:16:43 -0300

    reinserção de mecanismo de recomendação

--- lua/lazyvim/plugins/extras/lang/dotnet/easy-dotnet.lua
+++ lua/lazyvim/plugins/extras/lang/dotnet/easy-dotnet.lua
@@ -1,4 +1,11 @@
 return {
+  recommended = function()
+    return LazyVim.extras.wants({
+      ft = { "cs", "vb", "fsharp" },
+      root = { "*.sln", "*.csproj", "*.fsproj", "omnisharp.json", "function.json", "paket.dependencies", "paket.lock" },
+    })
+  end,
+
   { -- lazy.nvim
     "GustavEikaas/easy-dotnet.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "folke/snacks.nvim" },
