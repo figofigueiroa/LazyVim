380d62b3149550427da9c5c2d2be2a58c0b19942
Thiago Figueiroa Oliveira <thiagofigueiroaoliveira@gmail.com>
2026-06-04 08:51:28 -0300

    modifica specs do easy-dotnet

--- lua/lazyvim/plugins/extras/lang/dotnet/easy-dotnet.lua
+++ lua/lazyvim/plugins/extras/lang/dotnet/easy-dotnet.lua
@@ -1,11 +1,8 @@
 return {
-  recommended = function()
-    return LazyVim.extras.wants({
-      ft = { "cs", "vb", "fsharp" },
-      root = { "*.sln", "*.csproj", "*.fsproj", "omnisharp.json", "function.json", "paket.dependencies", "paket.lock" },
-    })
+  -- lazy.nvim
+  "GustavEikaas/easy-dotnet.nvim",
+  dependencies = { "nvim-lua/plenary.nvim", "folke/snacks.nvim" },
+  config = function()
+    require("easy-dotnet").setup()
   end,
-  {
-    "GustavEikaas/easy-dotnet.nvim",
-  },
 }
